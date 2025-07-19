97f8aa6aa058b376167f73672b7429b941371aa2
Adilkhan-111 <gulzhanyessetova@gmail.com>
2025-07-19 14:02:43 +0500

    initial commit
    half added mainScene

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    private void Update()
+    {
+        
+    }
+    public void RotateHolder()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+
+        }
+    }
+}
