425695e0f64691a58e7c0b0d6c5eb14f2770b354
Adilkhan-111 <gulzhanyessetova@gmail.com>
2025-07-20 10:27:34 +0500

    initial commit
    holder gamerplay are completed!

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -6,49 +6,63 @@ public class PlayerControl : MonoBehaviour
 {
     public int _index;
     [SerializeField] private float rotationSpeed;
-    [SerializeField]private float minRotZ = -1f;
-    [SerializeField]private float maxRotZ = -85f;
+    [SerializeField] private float minRotZ = -1f;
+    [SerializeField] private float maxRotZ = -85f;
+
+    private bool isReturning = false;
 
     private void Update()
     {
-        RotateHolder();
-    }
-    public void RotateHolder()
-    {
         if (Input.GetKey(KeyCode.Space))
         {
+            isReturning = false;
             CloseHolders();
-            
         }
-        if (Input.GetKeyUp(KeyCode.Space))
+        else if (Input.GetKeyUp(KeyCode.Space))
         {
-            transform.Rotate(new Vector3(0, 0, 0) * rotationSpeed * Time.deltaTime);
-            Debug.Log("True");
+            isReturning = true;
         }
-            
 
-
+        if (isReturning)
+        {
+            ReturnToZero();
+        }
     }
+
     public void CloseHolders()
     {
-        if(_index == 1)
+        if (_index == 1)
         {
             transform.Rotate(new Vector3(0, 0, -85) * rotationSpeed * Time.deltaTime);
             limitRot();
-
         }
-        if(_index == 2)
+        if (_index == 2)
         {
             transform.Rotate(new Vector3(0, 0, 85) * rotationSpeed * Time.deltaTime);
             limitRot();
         }
-        
     }
+
     public void limitRot()
     {
         Vector3 playerEulerAngles = transform.localRotation.eulerAngles;
         playerEulerAngles.z = (playerEulerAngles.z > 180) ? playerEulerAngles.z - 360 : playerEulerAngles.z;
         playerEulerAngles.z = Mathf.Clamp(playerEulerAngles.z, minRotZ, maxRotZ);
         transform.localRotation = Quaternion.Euler(playerEulerAngles);
+    }
+
+    private void ReturnToZero()
+    {
+        float currentZ = transform.localEulerAngles.z;
+        currentZ = (currentZ > 180) ? currentZ - 360 : currentZ;
+        float step = 50f * rotationSpeed * Time.deltaTime;
+        float newZ = Mathf.MoveTowards(currentZ, 0f, step);
+        transform.localRotation = Quaternion.Euler(0, 0, newZ);
+
+        // Остановить возврат, если достигли нуля
+        if (Mathf.Approximately(newZ, 0f))
+        {
+            isReturning = false;
+        }
     }
 }
