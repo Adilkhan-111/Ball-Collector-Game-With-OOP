e36d4bc3bfffbdb7039fe543017a6f47a39cc551
Adilkhan-111 <gulzhanyessetova@gmail.com>
2025-07-20 10:10:39 +0500

    initial commit
    half-ended gamerplay mehanics

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
+    public int _index;
+    [SerializeField] private float rotationSpeed;
+    [SerializeField]private float minRotZ = -1f;
+    [SerializeField]private float maxRotZ = -85f;
+
     private void Update()
     {
-        
+        RotateHolder();
     }
     public void RotateHolder()
     {
         if (Input.GetKey(KeyCode.Space))
         {
+            CloseHolders();
+            
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            transform.Rotate(new Vector3(0, 0, 0) * rotationSpeed * Time.deltaTime);
+            Debug.Log("True");
+        }
+            
+
+
+    }
+    public void CloseHolders()
+    {
+        if(_index == 1)
+        {
+            transform.Rotate(new Vector3(0, 0, -85) * rotationSpeed * Time.deltaTime);
+            limitRot();
 
         }
+        if(_index == 2)
+        {
+            transform.Rotate(new Vector3(0, 0, 85) * rotationSpeed * Time.deltaTime);
+            limitRot();
+        }
+        
+    }
+    public void limitRot()
+    {
+        Vector3 playerEulerAngles = transform.localRotation.eulerAngles;
+        playerEulerAngles.z = (playerEulerAngles.z > 180) ? playerEulerAngles.z - 360 : playerEulerAngles.z;
+        playerEulerAngles.z = Mathf.Clamp(playerEulerAngles.z, minRotZ, maxRotZ);
+        transform.localRotation = Quaternion.Euler(playerEulerAngles);
     }
 }
